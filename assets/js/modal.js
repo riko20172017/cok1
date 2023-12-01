51ae65749cf1d4ba6fd5d6da6f64f05bf820e780
riko20172017 <g_anz@mail.ru>
2023-12-01 16:25:24 +0300

    3

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,6 +1,16 @@
 function Modal({ toggle, children }) {
-    return <div className="modalWrapper"><button onClick={() => toggle()}>{children}</button>
+    return <div className="content-modal flex f-align-center f-justify-center" onClick={() => toggle()}>
+        <div className="modalWrapper">
+            <span className="text-right block">
+                <i onClick={() => toggle()} className="icon-close inline-block pointer">
+                </i>
+            </span>
+            <div className="modal-container">
 
+                {children}
+            </div>
+        </div>
+        {/* <button onClick={() => toggle()}></button> */}
     </div>
 }
 
