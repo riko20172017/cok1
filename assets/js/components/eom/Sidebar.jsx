63b1e32e1dfe43b79f9cee2f79b1ccffb0db1b15
anz <g_anz@mail.ru>
2023-12-13 17:20:52 +0300

    13

--- assets/js/components/eom/Sidebar.jsx
+++ assets/js/components/eom/Sidebar.jsx
@@ -2,30 +2,30 @@
 //
 
 export default ({ menuitems, setMenuel, setMenusel, menuel, menusel, eom }) => {
-    return <div className="topic-title">
-        {menuitems.map((el, j) => {
-            j++
-            return <div>
-                <div className={menuel == j && "menuopen"}>
-                    <a href={`/content/eom_${eom}/content/1.html#${j}`} target="frame">
-                        <button
-                            onClick={() => { setMenuel(j); setMenusel(1) }}
-                            className="topic-title-btn pointer">{j}. {el.title}</button>
-                    </a>
-                    <div className="page-title">
-                        {el.submenu.map((el, i) => {
-                            i++
-                            return <div>
-                                <a className={menusel == i && "submenuopen"}
-                                    onClick={() => setMenusel(i)}
-                                    href={`/content/eom_${eom}/content/1.html#${j}.${i}`}
-                                    target="frame">{j}.{i} {el.title}</a>
-                            </div>
-                        })}
-                    </div>
-                </div>
-                <div class="nodeSpacer"></div>
-            </div>
-        })}
-    </div>
+  return <div className="topic-title">
+    {menuitems.map((el, j) => {
+      j++
+      return <div>
+        <div className={menuel == j && "menuopen"}>
+          <a href={`/content/eom_${eom}/content/1.html#${j}`} target="frame">
+            <button
+              onClick={() => { setMenuel(j); setMenusel(1) }}
+              className="topic-title-btn pointer">{j}. {el.title}</button>
+          </a>
+          <div className="page-title">
+            {el.submenu.map((el, i) => {
+              i++
+              return <div>
+                <a className={menusel == i && "submenuopen"}
+                  onClick={() => setMenusel(i)}
+                  href={`/content/eom_${eom}/content/${j}.${i}.html`}
+                  target="frame">{j}.{i} {el.title}</a>
+              </div>
+            })}
+          </div>
+        </div>
+        <div class="nodeSpacer"></div>
+      </div>
+    })}
+  </div>
 }
