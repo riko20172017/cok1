0aa0a097ea26dd5a1b85166cff3e71197665f7eb
anz <g_anz@mail.ru>
2023-12-28 13:37:00 +0300

    17

---
+++ content/eom_3/assets/js/menuitems.js
@@ -0,0 +1,15 @@
+export default
+    [
+        {
+            title: "Тесты",
+            submenu:
+                [
+                    {
+                        title: "Тесты по HTML",
+                    },
+                    {
+                        title: "Тесты по CSS",
+                    },
+                ]
+        },
+    ]
